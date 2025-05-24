889701100e9b3a9b5e4c9b2d47e84dc0d4ca7866
Lukasz Sobczak <153393240+LukaszMateuszSobczak@users.noreply.github.com>
2025-05-24 11:31:05 +0200

    dynamic created products added, working on showing cart for purchase in css, working on creating cart in js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,41 @@
-import { menuArray } from "./data";
+import { menuArray } from './data.js';
+
+const createProducts = (productArray) => {
+    return productArray
+        .map((obj) => {
+            return `
+            <section class="product" data-product-id="${obj.id}">
+                <p class="product-img">${obj.emoji}</p>
+                <div class="product-description">
+                    <p class="product-name">${obj.name}</p>
+                    <p class="product-ingredients">${obj.ingredients.join(
+                        ', '
+                    )}</p>
+                    <p class="product-price">$${obj.price}</p>
+                </div>
+                <button class="product-add-cart" data-id="${
+                    obj.id
+                }">&#65122</button>
+            </section>
+            <div class="line"></div>
+        `;
+        })
+        .join('');
+};
+
+document.querySelector('#products-list-el').innerHTML =
+    createProducts(menuArray);
+
+const cartArray = [];
+
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.id) {
+        cartArray.push(
+            menuArray.find((obj) => {
+                return String(obj.id) === e.target.dataset.id;
+            })
+        );
+    }
+
+    console.log(cartArray);
+});
